c0aec59e3270dd565074acbeafdba93ca894ba08
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2021-10-19 08:07:22 +0300

    feat(optimizations): further optimizations

--- components/Head.tsx
+++ components/Head.tsx
@@ -14,16 +14,15 @@ export function Head({
     <NextHead>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <link rel="icon" href="/qr.png" />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Roboto:300,400,500,700&display=optional"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Merriweather:300,400,700|Roboto:300,400,700&display=optional"
-        rel="stylesheet"
-      />
       <meta name="viewport" content="initial-scale=1, width=device-width" />
+      
+      <link rel="icon" href="/qr.png" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Roboto:wght@300;400;500;700&display=swap"
+        rel="stylesheet"
+      />
 
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <script
