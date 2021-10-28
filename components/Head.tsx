98b891fd65a8f7e9bf9ee65fb6224cb887942776
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2021-10-29 01:46:07 +0300

    fix(head): update font weights

--- components/Head.tsx
+++ components/Head.tsx
@@ -21,7 +21,7 @@ export function Head({
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Roboto:wght@300;400;500;700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Roboto:wght@300;400;700&display=swap"
         rel="stylesheet"
       />
 
