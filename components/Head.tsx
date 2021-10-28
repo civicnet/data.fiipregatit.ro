81fce1ab1b4fd105542caeb7b3a4c7610eead0ea
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2021-10-29 01:45:01 +0300

    fix(head): use script optimization for ga

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NextHead from "next/head";
+import Script from 'next/script'
 
 type HeadProps = {
   title?: string;
@@ -24,12 +25,10 @@ export function Head({
         rel="stylesheet"
       />
 
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <script
-        async
+      <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
-      <script
+      <Script
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
