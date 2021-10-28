f01b731317d4b9d10a62aeb38df87c0da076e103
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2021-10-28 14:05:30 +0300

    feat(legend): expandable legend

--- components/FeatureMapLegend.tsx
+++ components/FeatureMapLegend.tsx
@@ -1,24 +1,51 @@
 import {
   Avatar,
+  Collapse,
   Divider,
+  IconButton,
+  IconButtonProps,
   List,
   ListItem,
   ListItemAvatar,
+  ListItemSecondaryAction,
   ListItemText,
   Paper,
+  styled,
   Typography,
   useTheme,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { SeverityLevel } from "../lib/SeverityLevel";
 import { SeverityLevelColor } from "../lib/SeverityLevelColor";
 import { SeverityLevelDescription } from "../lib/SeverityLevelDescription";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 type Props = {
   lastUpdatedAt: number;
 };
 
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 export default function FeatureMapLegend({ lastUpdatedAt }: Props) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   const theme = useTheme();
   return (
     <Paper
@@ -31,7 +58,7 @@ export default function FeatureMapLegend({ lastUpdatedAt }: Props) {
         zIndex: 10,
       }}
     >
-      <List dense={true} disablePadding sx={{ mt: 0 }}>
+      <List dense={true} disablePadding sx={{ mt: 0, width: 170 }}>
         <ListItem disablePadding>
           <ListItemText
             primary={
@@ -45,32 +72,46 @@ export default function FeatureMapLegend({ lastUpdatedAt }: Props) {
               })}
             `}
           />
+          <ListItemSecondaryAction sx={{ right: 0 }}>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </ListItemSecondaryAction>
         </ListItem>
-        <Divider />
-        {Object.entries(SeverityLevelColor.geojson).map(([k, v]) => {
-          return (
-            <ListItem disablePadding key={`severity-${k}`}>
-              <ListItemAvatar
-                sx={{
-                  minWidth: theme.spacing(3),
-                }}
-              >
-                <Avatar
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <Divider />
+          {Object.entries(SeverityLevelColor.geojson).map(([k, v]) => {
+            return (
+              <ListItem disablePadding key={`severity-${k}`}>
+                <ListItemAvatar
                   sx={{
-                    background: v,
-                    height: "1rem",
-                    width: "1rem",
+                    minWidth: theme.spacing(3),
                   }}
                 >
-                  {" "}
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary={SeverityLevelDescription.incidence[k as SeverityLevel]}
-              />
-            </ListItem>
-          );
-        })}
+                  <Avatar
+                    sx={{
+                      background: v,
+                      height: "1rem",
+                      width: "1rem",
+                    }}
+                  >
+                    {" "}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    SeverityLevelDescription.incidence[k as SeverityLevel]
+                  }
+                />
+              </ListItem>
+            );
+          })}
+        </Collapse>
       </List>
     </Paper>
   );
--- components/HospitalsMapLegend.tsx
+++ components/HospitalsMapLegend.tsx
@@ -1,20 +1,25 @@
 import {
   Avatar,
+  Collapse,
   Divider,
+  IconButton,
+  IconButtonProps,
   List,
   ListItem,
   ListItemAvatar,
   ListItemSecondaryAction,
   ListItemText,
   Paper,
+  styled,
   Switch,
   Tooltip,
   Typography,
   useTheme,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { SeverityLevel } from "../lib/SeverityLevel";
 import { SeverityLevelColor } from "../lib/SeverityLevelColor";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 type Props = {
   lastUpdatedAt: number;
@@ -22,6 +27,20 @@ type Props = {
   onFiltered: (level: SeverityLevel) => void;
   filtered: SeverityLevel[];
 };
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 export default function FeatureMapLegend({
   lastUpdatedAt,
@@ -29,7 +48,14 @@ export default function FeatureMapLegend({
   onFiltered,
   filtered,
 }: Props) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   const theme = useTheme();
+
   return (
     <Paper
       sx={{
@@ -41,8 +67,8 @@ export default function FeatureMapLegend({
         zIndex: 10,
       }}
     >
-      <List dense={true} disablePadding sx={{ mt: 0 }}>
-        <ListItem sx={{ pl: 0 }}>
+      <List dense={true} disablePadding sx={{ mt: 0, width: 250 }}>
+        <ListItem sx={{ pl: 0, pr: 0 }}>
           <ListItemText
             primary={
               <Typography sx={{ fontWeight: "bold" }}>Legendă</Typography>
@@ -55,82 +81,96 @@ export default function FeatureMapLegend({
                 })}
               `}
           />
-        </ListItem>
-        <Divider />
-        <ListItem sx={{ pl: 0, pb: 2 }}>
-          <ListItemText primary="Persoane spitalizate la ATI" />
-        </ListItem>
-        <ListItem disablePadding key={`severity-na`}>
-          <ListItemAvatar
-            sx={{
-              minWidth: theme.spacing(3),
-            }}
-          >
-            <Avatar
-              sx={{
-                background: "rgb(128, 128, 128)",
-                height: "1rem",
-                width: "1rem",
-              }}
-            >
-              {" "}
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="informații neactualizate" />
           <ListItemSecondaryAction sx={{ right: 0 }}>
-            <Tooltip title="Funcție indisponibilă" followCursor>
-              <span>
-                <Switch defaultChecked disabled size="small" />
-              </span>
-            </Tooltip>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
           </ListItemSecondaryAction>
         </ListItem>
-        {Object.entries(SeverityLevelColor.scatterplot).map(([k, v]) => {
-          return (
-            <ListItem disablePadding key={`severity-${k}`}>
-              <ListItemAvatar
+        <Divider />
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <ListItem sx={{ pl: 0, pb: 2 }}>
+            <ListItemText primary="Persoane spitalizate la ATI" />
+          </ListItem>
+          <ListItem disablePadding key={`severity-na`}>
+            <ListItemAvatar
+              sx={{
+                minWidth: theme.spacing(3),
+              }}
+            >
+              <Avatar
                 sx={{
-                  minWidth: theme.spacing(3),
+                  background: "rgb(128, 128, 128)",
+                  height: "1rem",
+                  width: "1rem",
                 }}
               >
-                <Avatar
+                {" "}
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="informații neactualizate" />
+            <ListItemSecondaryAction sx={{ right: 0 }}>
+              <Tooltip title="Funcție indisponibilă" followCursor>
+                <span>
+                  <Switch defaultChecked disabled size="small" />
+                </span>
+              </Tooltip>
+            </ListItemSecondaryAction>
+          </ListItem>
+          {Object.entries(SeverityLevelColor.scatterplot).map(([k, v]) => {
+            return (
+              <ListItem disablePadding key={`severity-${k}`}>
+                <ListItemAvatar
                   sx={{
-                    background: v,
-                    height: "1rem",
-                    width: "1rem",
+                    minWidth: theme.spacing(3),
                   }}
                 >
-                  {" "}
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary={
-                  k === SeverityLevel.NONE
-                    ? "zero persoane"
-                    : `între ${Math.round(limits[k as SeverityLevel][0])} și ${
-                      Math.round(limits[k as SeverityLevel][1])
-                      } persoane`
-                }
-              />
-              <ListItemSecondaryAction sx={{ right: 0 }}>
-                <Tooltip
-                  followCursor
-                  title={
-                    filtered.includes(k as SeverityLevel)
-                      ? "Nu afișa"
-                      : "Afișează"
+                  <Avatar
+                    sx={{
+                      background: v,
+                      height: "1rem",
+                      width: "1rem",
+                    }}
+                  >
+                    {" "}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    k === SeverityLevel.NONE
+                      ? "zero persoane"
+                      : `între ${Math.round(
+                          limits[k as SeverityLevel][0]
+                        )} și ${Math.round(
+                          limits[k as SeverityLevel][1]
+                        )} persoane`
                   }
-                >
-                  <Switch
-                    defaultChecked={filtered.includes(k as SeverityLevel)}
-                    size="small"
-                    onChange={() => onFiltered(k as SeverityLevel)}
-                  />
-                </Tooltip>
-              </ListItemSecondaryAction>
-            </ListItem>
-          );
-        })}
+                />
+                <ListItemSecondaryAction sx={{ right: 0 }}>
+                  <Tooltip
+                    followCursor
+                    title={
+                      filtered.includes(k as SeverityLevel)
+                        ? "Nu afișa"
+                        : "Afișează"
+                    }
+                  >
+                    <Switch
+                      defaultChecked={filtered.includes(k as SeverityLevel)}
+                      size="small"
+                      onChange={() => onFiltered(k as SeverityLevel)}
+                    />
+                  </Tooltip>
+                </ListItemSecondaryAction>
+              </ListItem>
+            );
+          })}
+        </Collapse>
       </List>
     </Paper>
   );
